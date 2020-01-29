5d02f892a185f25e181021dd1e1244ef9f116d2e
Zemichael Habtegiorgis <zahabt18@stlawu.edu>
2020-01-29 14:08:12 -0500

    exercise

--- javascript/javascriptExercise.js
+++ javascript/javascriptExercise.js
@@ -4,3 +4,16 @@ for (let i = 1; i <= 10 ; i++) {
   x += i**2;
 }
 console.log(x);
+
+
+i = 0
+j = 1
+total = 0
+
+while (total < 1000) {
+  total = i + j
+  i = j
+  j = total
+}
+
+console.log(i);
