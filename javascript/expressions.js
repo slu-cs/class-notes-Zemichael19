53843cb559fb79171abd81c2929918bb4ebe134c
Zemichael Habtegiorgis <zahabt18@stlawu.edu>
2020-01-29 13:37:05 -0500

    Javascript syntax

---
+++ javascript/expressions.js
@@ -0,0 +1,52 @@
+//Expression syntax in Javascript
+
+//Booleans
+true
+false
+
+//logic
+!false
+true&&false
+true||false
+
+//numbers (all one type, no ints or doubles)
+3.14
+
+//equality
+3===3.0
+3!==2.14
+
+//ordering
+3>2
+3>=2
+
+//strings
+'hello'
+
+//concatenation
+'a'+'b' === 'a+b'
+
+//arithmetic
+3+2 === 5
+3-2 === 1
+3*2 === 6
+3/2 === 1.5
+3%2 === 1
+3**2 === 9
+
+//type coercion
+'3' + 2 === '32'
+'2' + true === '2true'
+'3'-2 === 1
+3+true === 4
+
+//loose equality
+'3' == 3
+true == 1
+false == 0
+false == ''
+'' == 0
+
+//template strings
+
+`3+2 is ${3+2}`
