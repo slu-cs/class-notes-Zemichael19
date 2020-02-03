6671a4d75da403278136e20df150f1dde8c887a2
Zemichael Habtegiorgis <zahabt18@stlawu.edu>
2020-02-03 13:15:47 -0500

    javascript arrays

--- javascript/objects.js
+++ javascript/objects.js
@@ -3,3 +3,45 @@
 //object literal
 const point = {x: 1, y: 2}
 console.log(point);
+
+
+//accessing properties
+console.log(point.x);
+
+//changing properties
+point.x = 3;
+
+//nested object literal
+const circle = {
+  center: point, radius:4
+};
+
+
+//Access subproperties
+console.log(circle.center.x);
+
+//undefined subproperties
+console.log(circle.color); //undefined
+circle.color = 'blue';
+console.log(circle);
+
+//array
+const languages = ['HTML', 'CSS', 'JS']
+console.log(languages);
+
+//accessing length and elements
+for (let i = 0; i < languages.length; i++) {
+  console.log(langauges[i]);
+}
+
+
+//changing elements
+langauges[2] = 'Javascript';
+
+//adding elements
+langauges.push('MongoDB');
+
+//iterating over elements
+for (const lang of langauges) {
+  console.log(lang);
+}
