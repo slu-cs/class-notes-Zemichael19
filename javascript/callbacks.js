3a389c7fff28d3e70c8dbd28a939fe65d509f3ae
Zemichael Habtegiorgis <zahabt18@stlawu.edu>
2020-02-05 13:32:31 -0500

    Javascript arrow

--- javascript/callbacks.js
+++ javascript/callbacks.js
@@ -21,3 +21,11 @@ const numbers = [1,2,3,4,5];
 
 //doing the transformation
 console.log(map(numbers, square));
+console.log(numbers);
+
+//defining other transforms with the arrow function
+console.log(map(numbers, x => x**2));
+console.log(map(numbers, x=>x+1));
+
+//using the array map method
+console.log(numbers.map(x => x**2));
