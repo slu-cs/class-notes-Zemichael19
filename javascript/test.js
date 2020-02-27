39fa773f17ca50b2bdb1af27a1a42257890c3f04
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-02-27 14:37:49 -0500

    changes

---
+++ javascript/test.js
@@ -0,0 +1,19 @@
+
+
+const f1 = function() {
+  return Math.random();
+}
+console.log(f1);
+const f2 = function() {
+  return Math.random();
+}
+console.log(f2);
+Promise.all(f1, f2).then(function(f1, f2) {
+  if (f1>f2) {
+    console.log('f1')
+  } else if (f2>f1) {
+    console.log('f2');
+  } else {
+    console.log('equal');
+  }
+}).catch(error=>console.error(error.stack));
--- mongo/query.js
+++ mongo/query.js
@@ -31,19 +31,13 @@ const queries = [
   Professor.distinct('rank')
 ];
 
-//run the queries in parallel
+// Run the queries in parallel
 Promise.all(queries)
-  .then(function(results)) {
-
-}
-
-
-  // Run the queries in parallel
-  Promise.all(queries)
-    .then(function(results) {
-      console.log('Names in order: ', results[0].map(p => p.name));
-      console.log('Started most recently: ', results[1].map(p => p.name));
-      console.log('Started in 2003: ', results[2].map(p => p.name));
-      console.log('Teaches 362: ', results[3].map(p => p.name));
-      console.log('Distinct ranks: ', results[4]);
-    }).catch(error => console.error(error.stack));
+  .then(function(results) {
+    console.log('Names in order: ', results[0].map(p => p.name));
+    console.log('Started most recently: ', results[1].map(p => p.name));
+    console.log('Started in 2003: ', results[2].map(p => p.name));
+    console.log('Teaches 362: ', results[3].map(p => p.name));
+    console.log('Distinct ranks: ', results[4]);
+    mongoose.connection.close();
+  }).catch(error => console.error(error.stack));
