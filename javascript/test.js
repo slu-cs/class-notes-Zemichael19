b161b37d2fa7446666a29467f9f1cf0d43e59ef8
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-02-27 14:43:05 -0500

    changes

--- javascript/test.js
+++ javascript/test.js
@@ -1,20 +1,10 @@
 
 
 const f1 = function() {
-  return Math.random();
+  return 3;
 }
-
+console.log(f1);
 const f2 = function() {
-  return Math.random();
+  return 4;
 }
-
-
-Promise.all(f1, f2).then(function(f1, f2) {
-  if (f1>f2) {
-    console.log('f1')
-  } else if (f2>f1) {
-    console.log('f2');
-  } else {
-    console.log('equal');
-  }
-}).catch(error=>console.error(error.stack));
+console.log(f2);
