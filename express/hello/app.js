99746338f0aa07276ad24d4e14eaba5f80f72732
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-03-04 13:05:07 -0500

    Hello world

---
+++ express/hello/app.js
@@ -0,0 +1,15 @@
+//hello world web server
+const express = require('express');
+
+//create the server
+const app = express();
+
+
+//respond to a single request
+app.get('/', function(request, response) {
+  response.send('Hello Lisa');
+})
+
+//start the server
+app.listen(3000);
+console.log('Server is ready');
