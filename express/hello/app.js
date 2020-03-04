ddde3d37424e8bb80f909fc61855d20023da8e71
Zemichael Habtegiorgis <zemichaelhabtegiorgis@Zemichaels-MacBook-Pro.local>
2020-03-04 13:35:23 -0500

    express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -5,9 +5,29 @@ const express = require('express');
 const app = express();
 
 
-//respond to a single request
+//cs-linuxlab-19.stlawu.edu:3000/
 app.get('/', function(request, response) {
   response.send('Hello Mike');
+})
+
+//cs-linuxlab-19.stlawu.edu:3000/foo
+app.get('/foo', function(request, response) {
+  response.send('Hello foo');
+})
+
+//cs-linuxlab-19.stlawu.edu:3000/foo/bar
+app.get('/foo', function(request, response) {
+  response.send('Hello bar');
+});
+
+//cs-linuxlab-19.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(request, response) {
+  response.send(`Hello ${request.params.z}`);
+})
+
+//cs-linuxlab-19.stlawu.edu:3000/zap/?x=&y=
+app.get('/zap', function(request, response) {
+  response.send(`Hello zap with ${request.params.x} and ${request.params.y}`);
 })
 
 //start the server
